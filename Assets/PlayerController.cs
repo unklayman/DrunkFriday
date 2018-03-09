f79f2c8ecac5ff2b662b06725a0aa478ae39f067
Andrei Marin <aamarin.sbt@sberbank.ru>
2018-03-09 16:37:03 +0300

    network sync

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +12,20 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update()
 	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
 	}
 }
